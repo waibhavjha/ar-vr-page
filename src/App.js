3a43a473fa80147e3aac91109a8f756499cee057
Waibhav Jha <waibhav204@gmail.com>
2024-04-25 01:12:47 +0530

    update v2.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import HPage from './components/HPage';
-//import HomePage2 from './components/HomePage2';
-import Search from './components/Search';
-import LuxuryApartment from './components/LuxuryApartment';
-import Page from './components/Page';
-import Properties from './components/Properties';
-import Location from './components/Location';
-import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <HPage />
-      <Search />
-      <LuxuryApartment />
-      <Page />
-      <Properties />
-      <Location />
-      <Footer />
     </div>
   );
 };
