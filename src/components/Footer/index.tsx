d223675b65ec5986bbf9fa324dfb19855ca36510
Waibhav Jha <waibhav204@gmail.com>
2024-04-29 21:06:33 +0530

    update

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <>
-    
       <footer className="relative z-10 bg-white pt-16 dark:bg-gray-dark md:pt-20 lg:pt-24">
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
