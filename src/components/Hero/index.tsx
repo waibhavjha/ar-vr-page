1c3cdb30c043eb103d23cfb9ac8264fb725b7035
Waibhav Jha <waibhav204@gmail.com>
2024-05-04 13:07:00 +0530

    update

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -62,11 +62,12 @@ const Hero = () => {
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
-                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  India's ameliorated #SuperApp
+              <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                India&apos;s ameliorated #SuperApp
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                  Explore & Experience in 360°. Explore world at the comfort of your fingertips whenever, wherever you are. Visualize & Experience the real-world virtually enhanced like never before the world's powerful & leading advanced-tech, now in India.
+                Explore & Experience in 360°.
+                Explore world at the comfort of your fingertips whenever, wherever you are. Visualize & Experience the real-world virtually enhanced like never before the world&apos;s powerful & leading advanced-tech, now in India.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <a
